a8863685f8589d02d4ed8eb663248c152700d8c0
Sergey <asmcm88@gmail.com>
2023-04-20 04:15:32 +0300

    005

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")           
+{
+    Console.WriteLine("Здравствуйте, Мария! Рады вас снова видеть");       
+}
+else
+{   
+    Console.Write("Oh, it's you...");
+    Console.WriteLine(username);
+}
